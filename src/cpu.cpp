cc31b3a11af9d80dee723afefe4a9b45a8d6f164
poxford3 <ptobmo@gmail.com>
2026-05-03 21:42:54 -0500

    in case opcode separation was bad

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -4,10 +4,11 @@
 
 void Cpu::reset(Mem &memory) {
     // https://gbdev.io/pandocs/Power_Up_Sequence.html?highlight=boot#cpu-registers
+    // Initial values based on Game Boy
     PC = 0x0100;
     SP = 0xFFFE;    // Stack pointer starts at the end of memory
-    A = 0x01;       // Initial values based on Game Boy
-    F = 0xB0;       // Initial flags (Z=1, N=0, H=1, C=1) 1011 = 0xB0
+    A = 0x01;       // Accumulator
+    F = 0xB0;       // Initial flags (Z=1, N=0, H=1, C=1) 1011 = 0xB0, bottom 4 bits are always 0
     B = 0x00;
     C = 0x13;
     D = 0x00;
@@ -18,7 +19,17 @@ void Cpu::reset(Mem &memory) {
     memory.init(); // Initialize memory to 0
 }
 
+void Cpu::updateFlags(Byte result, bool isSubtraction, bool halfCarry, bool carry) {
+    bool flag_z = (result == 0); // Set Z flag if result is zero
+    bool flag_n = isSubtraction; // Set N flag for subtraction, reset for addition
+    bool flag_h = halfCarry; // Set H flag based on half carry
+    bool flag_c = carry; // Set C flag based on carry
+    F = (flag_z << 7) | (flag_n << 6) | (flag_h << 5) | (flag_c << 4); // Update flags in F register
+}
+
 void Cpu::showAllRegisterValues() {
+    Byte flags = F;
+    std::cout << "Flags: Z=" << ((flags & 0x80) ? "1" : "0") << " N=" << ((flags & 0x40) ? "1" : "0") << " H=" << ((flags & 0x20) ? "1" : "0") << " C=" << ((flags & 0x10) ? "1" : "0") << std::endl;
     std::cout << "AF: 0x" << std::hex << AF << std::dec << " (A: 0x" << std::hex << static_cast<int>(A) 
               << ", F: 0x" << std::hex << static_cast<int>(F) << std::dec << ")" << std::endl;
     std::cout << "BC: 0x" << std::hex << BC << std::dec << " (B: 0x" << std::hex << static_cast<int>(B) 
@@ -27,6 +38,8 @@ void Cpu::showAllRegisterValues() {
               << ", E: 0x" << std::hex << static_cast<int>(E) << std::dec << ")" << std::endl;
     std::cout << "HL: 0x" << std::hex << HL << std::dec << " (H: 0x" << std::hex << static_cast<int>(H) 
               << ", L: 0x" << std::hex << static_cast<int>(L) << std::dec << ")" << std::endl;
+    std::cout << "SP: 0x" << std::hex << SP << std::dec << std::endl;
+    std::cout << "PC: 0x" << std::hex << PC << std::dec << std::endl;
 }
 
 Byte Cpu::fetchInstruction(uint &cycles, Mem &memory) {
@@ -37,80 +50,344 @@ Byte Cpu::fetchInstruction(uint &cycles, Mem &memory) {
     return opcode;
 }
 
-Byte Cpu::readByte(uint &cycles, Mem &memory, Word address) {
+Byte Cpu::readByte(Mem &memory, Word address) {
     Byte value = memory[address];
-    // cycles--;
     return value;
 }
 
-Word Cpu::loadWord(uint &cycles, Mem &memory) {
+Word Cpu::loadWord(Mem &memory) {
     Byte low = memory[PC];
     PC++; // Increment program counter to point to the next byte
     Byte high = memory[PC];
     PC++; // Increment program counter to point to the next instruction
     Word value = (high << 8) | low;
-    // cycles -= 2; // Assuming reading a word takes 2 cycles
     return value;
 }
 
-Byte Cpu::loadByte(uint &cycles, Mem &memory) {
+Byte Cpu::loadByte(Mem &memory) {
     Byte value = memory[PC];
     PC++; // Increment program counter to point to the next instruction
-    // cycles--; // Assuming reading a byte takes 1 cycle
     return value;
 }
 
 Word Cpu::incWord(Word value) {
+    // todo, probably need to check this is the right implementation
     return (value + 1) & 0xFFFF; // Increment and wrap around at 0xFFFF
 }
 
-void Cpu::loadToMemory(uint &cycles, Mem &memory, Word address, Byte &reg) {
-    memory[address] = reg;
-    // cycles--; // Assuming memory access takes 1 cycle
+Word Cpu::decWord(Word value) {
+    return (value - 1) & 0xFFFF; // Decrement and wrap around at 0x0000
 }
 
-void Cpu::loadFromMemory(uint &cycles, Mem &memory, Word address, Byte &reg) {
+void Cpu::loadRegToMemory(Mem &memory, Word address, Byte &reg) {
+    memory[address] = reg;
+}
+
+void Cpu::loadRegFromMemory(Mem &memory, Word address, Byte &reg) {
     reg = memory[address];
-    // cycles--; // Assuming memory access takes 1 cycle
 }
 
 void Cpu::loadRegToReg(Byte &dest, Byte &src) {
     dest = src;
 }
 
+void Cpu::andRegToA(Byte &src) {
+    A &= src;
+    updateFlags(A, false, true, false); // Z flag is set based on result, N H C flags are reset
+}
+
+void Cpu::orRegToA(Byte &src) {
+    A |= src;
+    updateFlags(A, false, false, false); // Z flag is set based on result, N H C flags are reset
+}
+
+void Cpu::xorRegToA(Byte &src) {
+    A ^= src;
+    updateFlags(A, false, false, false); // Z flag is set based on result, N H C flags are reset
+}
+
+void Cpu::addRegToReg(Byte &dest, Byte &src) {
+    // todo
+    bool flag_h = ((dest & 0x0F) + (src & 0x0F)) > 0x0F; // Set H flag if there is a carry from bit 3
+    bool flag_c = (dest + src) > 0xFF; // Set C flag if there is a carry from bit 7
+    dest += src;
+    updateFlags(dest, false, flag_h, flag_c);
+}
+
+void Cpu::subRegToReg(Byte &dest, Byte &src) {
+    // todo
+    // flag_h = (dest & 0x0F) < (src & 0x0F); // Set H flag if there is a borrow from bit 4
+    // flag_c = dest < src; // Set C flag if there is a borrow from bit 8
+    // dest -= src;
+    // updateFlags(dest, true, flag_h, flag_c);
+}
+
+void Cpu::PopStackToReg(Word &reg, Mem &memory) {
+    reg = memory[SP] | (memory[SP + 1] << 8); // Pop low byte and high byte from stack
+    SP += 2; // Increment stack pointer by 2
+}
+
+void Cpu::PushRegToStack(Word reg, Mem &memory) {
+    SP -= 1;
+    memory[SP] = (reg >> 8) & 0xFF; // Push high byte onto stack
+    SP -= 1;
+    memory[SP] = reg & 0xFF; // Push low byte onto stack
+}
+
+
 void Cpu::executeInstruction(uint cycles, Mem &memory) {
     while (cycles > 0) {
         Byte opcode = fetchInstruction(cycles, memory);
-        std::cout << "cycles: " << cycles << std::endl;
+        // std::cout << "cycles: " << cycles << std::endl;
 
         switch (opcode) {
+            // x0 opcodes
             case NOP: {
                 std::cout << "Executed NOP" << std::endl;
                 cycles -= Cpu::opcycles[opcode] - 1;
                 break;
-            } 
-            case LD_BC_d16: {
-                BC = loadWord(cycles, memory);
+            }
+            case STOP: {
+                // TODO
+                std::cout << "Executed STOP" << std::endl;
                 cycles -= Cpu::opcycles[opcode] - 1;
-                std::cout << "Executed LD BC,d16 with value: 0x" << std::hex << BC << std::dec << std::endl;
+                break;
+            }
+            case JR_NZ_s8: {
+                // TODO
+                break;
+            }
+            case JR_NC_s8: {
+                // TODO
+                break;
+            }
+            case LD_B_B: {
+                cycles -= Cpu::opcycles[opcode] - 1; // effectively NOP
+                break;
+            }
+            case LD_D_B: {
+                loadRegToReg(D, B);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case LD_H_B: {
+                loadRegToReg(H, B);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case LD_HLmem_B: {
+                loadRegToMemory(memory, HL, B);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case ADD_A_B: {
+                addRegToReg(A, B);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case SUB_B: {
+                subRegToReg(A, B);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case AND_B: {
+                andRegToA(B);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case OR_B: {
+                orRegToA(B);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case RET_NZ: {
+                // TODO
+                break;
+            }
+            case RET_NC: {
+                // TODO
+                break;
+            }
+            case LD_a8mem_A: {
+                Byte a8 = loadByte(memory);
+                loadRegToMemory(memory, a8, A);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case LD_A_a8mem: {
+                Byte a8 = loadByte(memory);
+                loadRegFromMemory(memory, a8, A);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+
+            // x1 opcodes
+            case LD_BC_d16: {
+                BC = loadWord(memory);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case LD_DE_d16: {
+                DE = loadWord(memory);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case LD_HL_d16: {
+                HL = loadWord(memory);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case LD_SP_d16: {
+                SP = loadWord(memory);
+                cycles -= Cpu::opcycles[opcode] - 1;
                 break;
             }
             case LD_B_C: {
                 loadRegToReg(B, C);
                 cycles -= Cpu::opcycles[opcode] - 1;
-                std::cout << "Executed LD B,C with value: 0x" << std::hex << static_cast<int>(B) << std::dec << std::endl;
                 break;
             }
+            case LD_D_C: {
+                loadRegToReg(D, C);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case LD_H_C: {
+                loadRegToReg(H, C);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case LD_HLmem_C: {
+                loadRegToMemory(memory, HL, C);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case ADD_A_C: {
+                addRegToReg(A, C);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case SUB_C: {
+                subRegToReg(A, C);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case AND_C: {
+                andRegToA(C);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case OR_C: {    
+                orRegToA(C);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case POP_BC: {
+                PopStackToReg(BC, memory);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case POP_DE: {
+                PopStackToReg(DE, memory);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case POP_HL: {
+                PopStackToReg(HL, memory);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case POP_AF: {
+                PopStackToReg(AF, memory);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+
+            // x2 opcodes
             case LD_BCmem_A: {
-                loadFromMemory(cycles, memory, BC, A);
+                loadRegToMemory(memory, BC, A);
                 cycles -= Cpu::opcycles[opcode] - 1;
-                std::cout << "Executed LD (BC),A with value: 0x" << std::hex << static_cast<int>(A) << std::dec << std::endl;
                 break;
             }
-            case LD_A_BCmem: {
-                loadToMemory(cycles, memory, BC, A);
+            case LD_DEmem_A: {
+                loadRegToMemory(memory, DE, A);
                 cycles -= Cpu::opcycles[opcode] - 1;
-                std::cout << "Executed LD A,(BC) with value: 0x" << std::hex << static_cast<int>(A) << std::dec << std::endl;
+                break;
+            }
+            case LD_HLi_A: {
+                loadRegToMemory(memory, HL, A);
+                HL = incWord(HL);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case LD_HLd_A: {
+                loadRegToMemory(memory, HL, A);
+                HL = decWord(HL);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case LD_B_D: {
+                loadRegToReg(B, D);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case LD_D_D: {
+                cycles -= Cpu::opcycles[opcode] - 1; // effectively NOP
+                break;
+            }
+            case LD_H_D: {
+                loadRegToReg(H, D);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case LD_HLmem_D: {
+                loadRegToMemory(memory, HL, D);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case ADD_A_D: {
+                addRegToReg(A, D);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case SUB_D: {
+                subRegToReg(A, D);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case AND_D: {
+                andRegToA(D);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case OR_D: {
+                orRegToA(D);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case JP_NZ_a16: {
+                // TODO
+                break;
+            }
+            case JP_NC_a16: {
+                // TODO
+                break;
+            }
+            case LD_Cmem_A: {
+                loadRegToMemory(memory, C, A);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+            case LD_A_Cmem: {
+                loadRegFromMemory(memory, C, A);
+                cycles -= Cpu::opcycles[opcode] - 1;
+                break;
+            }
+
+
+            case LD_A_BCmem: {
+                loadRegToMemory(memory, BC, A);
+                cycles -= Cpu::opcycles[opcode] - 1;
                 break;
             }
             default:
--- src/main.cpp
+++ src/main.cpp
@@ -8,28 +8,29 @@
 
 int main() {
 
-
-    uint exampleOpcodes[4] = {0x00, 0x01, 0x34, 0x12}; // NOP, LD BC, 0x1234
-
     Mem memory;
     Cpu cpu;
     cpu.reset(memory);
 
+
+    opcodes opcodes; // test
+    std::cout << "NOP_test value from opcodes struct: " << opcodes.NOP_test << std::endl;
+
     // test
-    memory[0x0100] = Cpu::NOP; // NOP
-    memory[0x0101] = Cpu::LD_BC_d16; // LD BC,d16
-    memory[0x0102] = 0x34; // low byte of d16
-    memory[0x0103] = 0x12; // high byte of d16
+    // memory[0x0100] = Cpu::NOP; // NOP
+    // memory[0x0101] = Cpu::LD_BC_d16; // LD BC,d16
+    // memory[0x0102] = 0x34; // low byte of d16
+    // memory[0x0103] = 0x12; // high byte of d16
 
-    cpu.executeInstruction(4, memory);
-    cpu.showAllRegisterValues();
-    printf("\n\n");
+    // cpu.executeInstruction(4, memory);
+    // cpu.showAllRegisterValues();
+    // printf("\n\n");
 
-    memory[0x0104] = Cpu::LD_B_C; // LD B,C
+    // memory[0x0104] = Cpu::LD_B_C; // LD B,C
     
-    cpu.executeInstruction(1, memory);
-    cpu.showAllRegisterValues();
-    printf("\n\n");
+    // cpu.executeInstruction(1, memory);
+    // cpu.showAllRegisterValues();
+    // printf("\n\n");
     
     // memory[0x0105] = Cpu::LD_BCmem_A;
     // cpu.A = 0x42;
@@ -40,11 +41,23 @@ int main() {
     // printf("\n\n");
     
     // cpu.reset(memory);
-    memory[0x0100] = Cpu::LD_A_BCmem; // LD A,(BC)
-    cpu.A = 0x69;
-    memory[0x0013] = 0x42; // default BC is 0x0013
-    cpu.executeInstruction(2, memory);
-    std::cout << "Value at memory address 0x0013 after LD A,(BC): 0x" << std::hex << static_cast<int>(memory[0x0013]) << std::dec << std::endl;
+    // memory[0x0100] = Cpu::LD_A_BCmem; // LD A,(BC)
+    // cpu.A = 0x69;
+    // memory[0x0013] = 0x42; // default BC is 0x0013
+    // cpu.executeInstruction(2, memory);
+    // std::cout << "Value at memory address 0x0013 after LD A,(BC): 0x" << std::hex << static_cast<int>(memory[0x0013]) << std::dec << std::endl;
+    // cpu.showAllRegisterValues();
+
+    memory[0x0100] = Cpu::AND_B; // AND B with A
+    cpu.B = 0xB1;
+    cpu.A = 0xB4;
+    cpu.executeInstruction(Cpu::opcycles[Cpu::AND_B], memory);
+    cpu.showAllRegisterValues();
+    printf("\n\n");
+    cpu.A = 0x90;
+    cpu.B = 0x80;
+    memory[0x0101] = Cpu::ADD_A_B;
+    cpu.executeInstruction(Cpu::opcycles[Cpu::ADD_A_B], memory);
     cpu.showAllRegisterValues();
     // end test
 
